4d276c7a3054c0a0fc549d3a739e656a64134cc2
abey <bruckabey@gmail.com>
2026-07-05 16:25:21 +0930

    drizzle completly removed from project

--- src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-
-import postgres from "postgres";
-
-import { drizzle } from "drizzle-orm/postgres-js";
-
-const client = postgres(process.env.DATABASE_URL!);
-
-export const db = drizzle(client);
